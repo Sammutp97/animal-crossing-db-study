350a867c09c561c4d5623358073b688824f41b90
Antoine Stevan <stevan.antoine@gmail.com>
2021-10-24 16:44:18 +0200

    Cleaner in the parser.

--- scripts/imports/redis_import_main.py
+++ scripts/imports/redis_import_main.py
@@ -1,11 +1,12 @@
 import argparse
 import os
+import traceback
 
 import redis
 import csv
 import glob
 
-def main(db_path, script=True, auto=False, verbose=False):
+def main(db_path, script=False, auto=False, verbose=False, clean=False):
     # gets the name of the directory where the script is, to allow
     # executing the script from anywhere.
     directory = os.path.dirname(os.path.realpath(__file__))
@@ -13,11 +14,23 @@ def main(db_path, script=True, auto=False, verbose=False):
     #DATABASE IMPORT TO REDIS
     csv_files = [f for f in glob.glob(os.path.join(db_path, "*.csv"))]
     elem_id = 0
+    output_filename = os.path.join(directory, "import_db.redis")
+
+    if clean:
+        if os.path.exists(output_filename):
+            print(f"removing {output_filename}")
+            os.remove(output_filename)
+        else:
+            print("no .redis file to remove... Skipping.")
+
+    if not script and not auto:
+        print("nothing to do... Aborting.")
+        return
 
     if auto: #Open Redis DBM
         r = redis.Redis() #host='localhost', port=6379, db=0, password=None, socket_timeout=None
     if script:
-        redis_script = open(os.path.join(directory, "import_db.redis"), "w")
+        redis_script = open(output_filename, "w")
 
     for csv_file in csv_files:
         #Read CSV File
@@ -49,7 +62,7 @@ def main(db_path, script=True, auto=False, verbose=False):
 
             csvfile.close()
         except:
-            print(f'WARNING: the file {csv_file} was not read correctly. Possibily due to some UTF-8 character.')
+            print(traceback.format_exc())
 
     if script:
         redis_script.close()
@@ -59,21 +72,24 @@ if __name__ == "__main__":
     # create a parser to change behaviour from outside.
     parser = argparse.ArgumentParser()
     parser.add_argument("tables", type=str, nargs=1,
-            help="if raised, the execution won't generate a redis import script (defaults to False).")
-    parser.add_argument('-s', "--no-script", default=False, action="store_true",
-            help="if raised, the execution won't generate a redis import script (defaults to False).")
+            help="the location of the tables, from the execution location (is required).")
+    parser.add_argument('-s', "--script", default=False, action="store_true",
+            help="if raised, the execution will generate a redis import script (defaults to False).")
     parser.add_argument('-a', "--auto", default=False, action="store_true",
             help="allows the script to automatically load the database inside a redis server. make sure a redis server is activated before hand. (defaults to False).")
     parser.add_argument('-v', "--verbose", default=False, action="store_true",
             help="turns on verbose (defaults to False).")
+    parser.add_argument('-c', "--clean", default=False, action="store_true",
+            help="cleans the workspace from generated .redis file (defaults to False).")
 
     args = parser.parse_args()
 
     #EXECUTION PARAMETERS
-    script = not args.no_script
+    script = args.script
     auto = args.auto
     verbose = args.verbose
     db_path = args.tables[0]
+    clean = args.clean
 
     # the actual redis database generation.
-    main(db_path, script=script, auto=auto, verbose=verbose)
+    main(db_path, script=script, auto=auto, verbose=verbose, clean=clean)
